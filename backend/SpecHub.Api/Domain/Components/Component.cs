c15cb3ae8512fe1383ecbe883ef6d091bd6cbcb9
torokur93 <torokur93@gmail.com>
2026-09-19 19:45:25 +0200

    [FIX] API - ComponentType typo

--- backend/SpecHub.Api/Domain/Components/Component.cs
+++ backend/SpecHub.Api/Domain/Components/Component.cs
@@ -5,7 +5,7 @@ namespace SpecHub.Api.Components;
 
 [JsonPolymorphic(TypeDiscriminatorPropertyName = "type")]
 [JsonDerivedType(typeof(TitleComponent), "title")]
-[JsonDerivedType(typeof(ParagraphComponent), "paragrah")]
+[JsonDerivedType(typeof(ParagraphComponent), "paragraph")]
 [JsonDerivedType(typeof(TrueOrFalseComponent), "true_false")]
 public abstract class Component
 {
