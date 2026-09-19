149c135327e1ef09281ca716836bcc820a42f57d
torokur93 <torokur93@gmail.com>
2026-09-19 18:06:46 +0200

    [ADD] API - Document, Module, Component

---
+++ backend/SpecHub.Api/Domain/Documents/Document.cs
@@ -0,0 +1,42 @@
+using SpecHub.Api.Modules;
+using System.Text.Json.Serialization;
+
+namespace SpecHub.Api.Documents;
+
+public class Document
+{
+    public Guid Id { get; private set; }
+    public int Version { get; private set; }
+
+    [JsonPropertyName("created_at")]
+    public DateTime CreatedAt { get; private set; }
+
+    [JsonPropertyName("created_by")]
+    public string CreatedBy { get; private set; }
+
+    [JsonPropertyName("last_modified")]
+    public DateTime LastModified { get; private set; }
+
+    public List<Module> Modules { get; private set; }
+
+    public Document() : this(Guid.NewGuid(), 0, DateTime.Now, String.Empty, DateTime.Now){}
+
+    public Document(Guid id, int version, DateTime createdAt, string createdBy, DateTime lastModified)
+    {
+        Id = id;
+        Version = version;
+        CreatedAt = createdAt;
+        CreatedBy = createdBy;
+        LastModified = lastModified;
+        Modules = new List<Module>();
+    }
+
+    public void AddModule(Module module)
+    {
+        if(module == null) {return;}
+
+        Modules.Add(module);
+
+    }
+
+}
---
+++ backend/SpecHub.Api/Domain/Modules/ModuleFrameType.cs
@@ -0,0 +1,10 @@
+
+namespace SpecHub.Api.Modules;
+
+public enum ModuleFrameType
+{
+    None,
+    Solid,
+    Dashed,
+    Dotted
+}
